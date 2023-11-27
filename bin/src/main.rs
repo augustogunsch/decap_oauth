c9d5d82d06cf0e6deebe5904588f10f1c5a09b44
Augusto Gunsch <augusto@augustogunsch.com>
2023-11-27 21:47:34 +0100

    Add error handling

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -1,9 +1,21 @@
 use axum::Router;
 use oauth_decap_github_lib::oauth_router;
+use std::env;
+use std::process::exit;
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
+    if let Err(_) = env::var("CLIENT_ID") {
+        eprintln!("error: undefined environment variable `CLIENT_ID`.");
+        exit(1);
+    }
+
+    if let Err(_) = env::var("SECRET") {
+        eprintln!("error: undefined environment variable `SECRET`.");
+        exit(1);
+    }
+
     let app = Router::new().merge(oauth_router());
 
     let listener = TcpListener::bind("0.0.0.0:3005").await.unwrap();
